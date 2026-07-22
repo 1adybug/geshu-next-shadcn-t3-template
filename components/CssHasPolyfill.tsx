2acd69d0e51ffc8ae1a12f29b94e3bd5d981e386
陆子虚 <lurongv@qq.com>
2026-07-22 16:06:07 +0800

    style: 扩展上游箭头函数格式规则

--- components/CssHasPolyfill.tsx
+++ components/CssHasPolyfill.tsx
@@ -24,17 +24,13 @@ function initializeCssHasPolyfill() {
     if (CSS.supports("selector(:has(*))")) return Promise.resolve()
     if (initializePromise) return initializePromise
 
-    initializePromise = import("css-has-pseudo/browser").then(({ default: cssHasPseudo }) => {
-        cssHasPseudo(document, { observedAttributes })
-    })
+    initializePromise = import("css-has-pseudo/browser").then(({ default: cssHasPseudo }) => void cssHasPseudo(document, { observedAttributes }))
 
     return initializePromise
 }
 
 export const CssHasPolyfill: FC = () => {
-    useEffect(() => {
-        void initializeCssHasPolyfill()
-    }, [])
+    useEffect(() => void void initializeCssHasPolyfill(), [])
 
     return null
 }
